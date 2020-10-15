5b03c033a40e18b28be42aefcd8096f8c217a910
evgheni.calcutin <evgheni.calcutin@codefactorygroup.com>
2020-10-15 17:50:28 +0300

    Fix webpack

---
+++ webpack.dev.ts
@@ -0,0 +1,49 @@
+import path from "path";
+import { Configuration } from "webpack";
+import HtmlWebPackPlugin from "html-webpack-plugin";
+
+interface WebpackConfig extends Configuration {
+  devServer: {
+    historyApiFallback: boolean;
+    port: number;
+  };
+}
+
+const config: WebpackConfig = {
+  mode: "development",
+  entry: "./src/main.tsx",
+  devtool: "source-map",
+  devServer: {
+    historyApiFallback: true,
+    port: 8080,
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?/,
+        loader: "awesome-typescript-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+export default config;
